3aa080781a94441d74a4ea4b5b70342677d1ecdd
Your Name <you@example.com>
2023-01-06 10:38:25 +0100

    Initial Commit

---
+++ marvel_app/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  marvel_app
+//
+//  Created by user225687 on 1/6/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ marvel_app/marvel_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  marvel_appApp.swift
+//  marvel_app
+//
+//  Created by user225687 on 1/6/23.
+//
+
+import SwiftUI
+
+@main
+struct marvel_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
